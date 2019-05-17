d866800cbb02defb103023ebea11726222dbf68c
vineel <sonnathivinil@gmail.com>
2019-05-17 15:14:20 -0400

    add ct's own carrier seed

---
+++ db/seedfiles/carrier_seed_ct.rb
@@ -0,0 +1,17 @@
+puts "*"*80
+puts "::: Generating MA Carriers:::"
+
+hbx_office = OfficeLocation.new(
+    is_primary: true,
+    address: {kind: "work", address_1: "address_placeholder", address_2: "address_2", city: "City", state: "St", zip: "10001" },
+    phone: {kind: "main", area_code: "111", number: "111-1111"}
+  )
+
+org = Organization.new(fein: "061475928", legal_name: "Anthem BlueCross BlueShield", office_locations: [hbx_office])
+cp = org.create_carrier_profile(id: "53e67210eb89914603000029", abbrev: "ABCBS", hbx_carrier_id: 20014, ivl_health: false, ivl_dental: false, shop_health: true, shop_dental: false, issuer_hios_ids: ['86545'], offers_sole_source: true)
+
+org = Organization.new(fein: "237442369", legal_name: "ConnectiCare", office_locations: [hbx_office])
+cp = org.create_carrier_profile(id: "53e67210eb89914603000022", abbrev: "CCARE", hbx_carrier_id: 20015, ivl_health: false, ivl_dental: false, shop_health: true, shop_dental: false, issuer_hios_ids: ['76962'], offers_sole_source: true)
+
+puts "::: Generated MA Carriers :::"
+puts "*"*80
