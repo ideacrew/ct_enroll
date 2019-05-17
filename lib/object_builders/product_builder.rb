30b4b25f349baa7bf9fc89682f160927e945cd21
vineel <sonnathivinil@gmail.com>
2019-05-17 16:26:28 -0400

    missing comma

--- lib/object_builders/product_builder.rb
+++ lib/object_builders/product_builder.rb
@@ -101,7 +101,7 @@ class ProductBuilder
         all_attributes = if is_health_product?
           {
             health_plan_kind: @qhp.plan_type.downcase,
-            ehb: @qhp.ehb_percent_premium.present? ? @qhp.ehb_percent_premium : 1.0
+            ehb: @qhp.ehb_percent_premium.present? ? @qhp.ehb_percent_premium : 1.0,
             product_package_kinds: [:single_product, :single_issuer, :metal_level]
           }
         else
